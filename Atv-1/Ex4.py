9af9027b63ca91e441f7398b640e118adb1e5ceb
LukaZSH <lukaalvesclaro@gmail.com>
2026-01-08 19:42:44 -0300

    Exercícios da Atv 1

---
+++ Atv-1/Ex4.py
@@ -0,0 +1,10 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Nome do produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade, "unidades")
+print("Preço total: R$", preco_total)
